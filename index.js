098b56c10c2bcdd044d02bded8e441b19cdb6964
Your Name <your.email@example.com>
2026-01-04 03:04:14 +0600

    all

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const port = process.env.PORT || 3000
 app.use(
   cors({
     origin: ["http://localhost:5173","https://diussm.vercel.app"], 
-    credentials: true,            
+    credentials: true,   
+    methods: ["GET", "POST", "PUT", "DELETE"]         
   })
 );
 app.use(express.json())
