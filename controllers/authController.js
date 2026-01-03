95a89f0fead3e521b1404f6bd6a45b7befd11736
Your Name <your.email@example.com>
2026-01-04 03:08:23 +0600

    oke

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,7 @@ try {
     res.cookie('token', token,{
         httpOnly: true,
         secure: true,
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
  return res.json({success:true, message:'Login Successfull', user})
